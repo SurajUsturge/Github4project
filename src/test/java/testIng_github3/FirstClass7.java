61d3595ed9ff73047fc9264edb546c60f207421f
suraj <suraj.u9903@gmail.com>
2024-01-26 17:44:52 +0530

    commiting 2 files to new branch

--- src/test/java/testIng_github3/FirstClass7.java
+++ src/test/java/testIng_github3/FirstClass7.java
@@ -6,5 +6,6 @@ public void tester()
 System.out.println("this code is commited to master branch-done");
 System.out.println("now clone this code from master branch to new git repository in eclipse- done");
 System.out.println("then Import git project to eclipse workspace- done");
+
 }
 }
